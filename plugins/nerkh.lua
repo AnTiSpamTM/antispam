5c51a6d62e5d87a50538912c1aa84eb215e627b1
AnTiSpamTM <anti_spam1@yahoo.com>
2016-08-27 23:03:31 +0430

    Add files via upload

---
+++ plugins/nerkh.lua
@@ -0,0 +1,36 @@
+do
+
+function run(msg, matches)
+  return [[ 
+❗️نرخ خرید ربات AnTi_SPAM❗️
+💲1 ماهه 3 هزار تومان 💲
+💲2 ماهه 6 هزار تومان 💲
+💲3 ماهه 9 هزار تومان 💲
+💲همیشگی 15 هزار تومان💲
+
+لینک ساپورت ربات ⚜AnTi_SPAM⚜
+
+https://telegram.me/joinchat/C8yGfUAinzHpeiIN-ZHszA
+
+لینک کانال ربات 
+@AnTiSpamTM
+]]
+end
+
+return {
+  description = "Shows bot version", 
+  usage = "ver: Shows bot version",
+  patterns = {
+    "^[Nn]erkh$",
+    "^[Nn]erkh$",
+    "^!nerkh$",
+    "^%نرخ$",
+   "^#nerkh$",
+   "^#nerkh$",
+   "^/nerkh$",
+   "^#nerkh$",
+  }, 
+  run = run 
+}
+
+end
